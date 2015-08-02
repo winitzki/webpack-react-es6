42a4b1a60dc87465f3b189c7ab68452176751d2c
Sergei Winitzki <sergei@versal.com>
2015-08-01 19:43:13 -0700

    setting up ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 var path = require('path');
-var HtmlwebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
 
+var TARGET = process.env.TARGET;
 var ROOT_PATH = path.resolve(__dirname);
 
-module.exports = {
+var common_settings = {
   entry: path.resolve(ROOT_PATH, 'app/index'),
   output: {
     path: path.resolve(ROOT_PATH, 'build'),
@@ -19,8 +21,28 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlwebpackPlugin({
-      title: 'Kanban app'
+    new HtmlWebpackPlugin({
+      title: 'test app 1'
     })
   ]
 };
+
+if(TARGET === 'dev') {
+  module.exports = merge(common_settings, {
+    devtool: 'eval',
+    module: {
+      preLoaders: [
+        {
+          test: /\.jsx?$/,
+          // we are using `eslint-loader` explicitly since
+          // we have ESLint module installed. This way we
+          // can be certain that it uses the right loader
+          loader: 'eslint-loader',
+          include: path.resolve(ROOT_PATH, 'app')
+        }
+      ]
+    }
+  });
+} else {
+  module.exports = common;
+}
